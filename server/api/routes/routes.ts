3d8242aaa59f00a4ac6b54314f30d5effe1e8ffc
DiegoDulval <diegodifreitas@gmail.com>
2018-04-02 14:25:36 -0300

    Create initial files for Product module

--- server/api/routes/routes.ts
+++ server/api/routes/routes.ts
@@ -1,15 +1,26 @@
 import { Application, Request, Response } from 'express';
+import ProductRoutes from '../../modules/products/router';
 
 class Routes {
+
+    private router: ProductRoutes;
+
     constructor(app: Application) {
+        this.router = new ProductRoutes();
         this.getRoutes(app);
     }
 
     getRoutes(app: Application): void {
-        app.route('/')
-            .get((req: Request, res: Response) => res.send('Ta funcionando!'))
-        app.route('/ola/:nome')
-            .get((req: Request, res: Response) => res.send(`Eai, ${req.params.nome}`))
+        app.route('/api/produtos')
+            .get(this.router.findAll);
+        app.route('/api/produtos')
+            .post(this.router.create);
+        app.route('/api/produtos/:id')
+            .get(this.router.findOne);
+        app.route('/api/produtos/:id')
+            .put(this.router.update);
+        app.route('/api/produtos/:id')
+            .delete(this.router.destroy);
     }
 }
 
---
+++ server/modules/products/router.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express';
+
+class ProductRoutes {
+    constructor(){}
+
+    findAll(req: Request, res: Response){}
+    findOne(req: Request, res: Response){}
+    create(req: Request, res: Response){}
+    update(req: Request, res: Response){}
+    destroy(req: Request, res: Response){}
+}
+
+export default ProductRoutes;
