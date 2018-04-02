bdee4458f79be9d26932debf344d8d649bab5f39
DiegoDulval <diegodifreitas@gmail.com>
2018-04-02 17:10:52 -0300

    Created controller for Procut resources

--- server/modules/products/controller.ts
+++ server/modules/products/controller.ts
@@ -1,32 +1,69 @@
 import { Request, Response } from 'express';
+import * as HTTPStatus from 'http-status';
 
-class ProductController{
-    constructor(){}
+import Product from './service';
 
-    getAll(req: Request, res: Response){
-        res.status(200).json({
-            message: 'Ok'
-        })
+class ProductController {
+    constructor() { }
+
+    getAll(req: Request, res: Response) {
+        Product.getAll()
+            .then(data => {
+                res.status(HTTPStatus.OK).json({
+                    payload: data
+                })
+            })
+            .catch(err => {
+                res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({ payload: 'Erro ao buscar' })
+            });
     }
-    createProduct(req: Request, res: Response){
-        res.status(200).json({
-            message: 'Ok'
-        })
+    createProduct(req: Request, res: Response) {
+        Product.create(req.body)
+            .then(data => {
+                res.status(HTTPStatus.OK).json({
+                    payload: data
+                })
+            })
+            .catch(err => {
+                res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({ payload: 'Erro ao criar' })
+            });
     }
-    getById(req: Request, res: Response){
-        res.status(200).json({
-            message: 'Ok'
-        })
+    getById(req: Request, res: Response) {
+        const productId = parseInt(req.params.id);
+        Product.getById(productId)
+            .then(data => {
+                res.status(HTTPStatus.OK).json({
+                    payload: data
+                })
+            })
+            .catch(err => {
+                res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({ payload: 'Erro ao buscar' })
+            });
     }
-    updateProduct(req: Request, res: Response){
-        res.status(200).json({
-            message: 'Ok'
-        })
+    updateProduct(req: Request, res: Response) {
+        const productId = parseInt(req.params.id);
+        const props = req.body;
+        Product.update(productId, props)
+            .then(data => {
+                res.status(HTTPStatus.OK).json({
+                    payload: data
+                })
+            })
+            .catch(err => {
+                res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({ payload: 'Erro ao atualizar' })
+            });
     }
-    deleteProduct(req: Request, res: Response){
-        res.status(200).json({
-            message: 'Ok'
-        })
+    deleteProduct(req: Request, res: Response) {
+        const productId = parseInt(req.params.id);
+        Product.delete(productId)
+            .then(data => {
+                res.status(HTTPStatus.OK).json({
+                    payload: data
+                })
+            })
+            .catch(err => {
+                res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({ payload: 'Erro ao deletar' })
+            });
     }
 }
 
