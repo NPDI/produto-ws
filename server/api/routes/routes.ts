d20ac3fa771fd5f15e8c60118a9a9549a43f77bf
DiegoDulval <diegodifreitas@gmail.com>
2018-04-02 13:43:34 -0300

    Create initial routes

---
+++ server/api/routes/routes.ts
@@ -0,0 +1,16 @@
+import { Application, Request, Response } from 'express';
+
+class Routes {
+    constructor(app: Application) {
+
+     }
+
+     getRoutes(app: Application): void{
+         app.route('/')
+            .get((req: Request, res: Response) => res.send('Ta funcionando!'))
+         app.route('/ola/:nome')
+            .get((req: Request, res: Response) => res.send(`Eai, ${req.params.nome}`))
+     }
+}
+
+export default Routes;
